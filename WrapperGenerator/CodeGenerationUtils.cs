e033eb0b8ddb870e2a29e9b3cab8df6748fd41ab
Ricky <ricky@rpgrafx.com>
2024-05-28 22:05:12 -0700

    Consolidated Typing functions

---
+++ WrapperGenerator/CodeGenerationUtils.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace WrapperGenerator
+{
+    public static class CodeGenerationUtils
+    {
+        public static string GetBaseTypeName(Type type)
+        {
+            string baseName = type.Name;
+            if (type.IsGenericType)
+            {
+                baseName = type.Name.Substring(0, type.Name.IndexOf('`'));
+            }
+
+            return baseName;
+        }
+
+        public static string GetFriendlyName(Type type)
+        {
+            if (type.IsGenericType)
+            {
+                string genericType = type.GetGenericTypeDefinition().Name;
+                genericType = genericType.Substring(0, genericType.IndexOf('`'));
+                string genericArgs = string.Join(", ", type.GetGenericArguments().Select(GetFriendlyName));
+                return $"{genericType}<{genericArgs}>";
+            }
+
+            if (type.IsByRef)
+            {
+                return GetFriendlyName(type.GetElementType());
+            }
+
+            if (type.IsInterface && type.Namespace != null && type.Namespace.StartsWith("Steamworks"))
+            {
+                return "I" + type.Name;
+            }
+
+            return type.Name;
+        }
+
+        public static string GetGenericType(Type type)
+        {
+            if (type.IsGenericType)
+            {
+                return GetFriendlyName(type.GetGenericArguments()[0]);
+            }
+
+            return "object";
+        }
+
+        public static IEnumerable<string> GetMethodNamespaces(Type type)
+        {
+            foreach (MethodInfo method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance |
+                                                          BindingFlags.DeclaredOnly))
+            {
+                yield return method.ReturnType.Namespace;
+                foreach (ParameterInfo parameter in method.GetParameters())
+                {
+                    yield return parameter.ParameterType.Namespace;
+                }
+            }
+        }
+
+        public static string GenerateMethodSignature(MethodInfo method)
+        {
+            string returnType = GetFriendlyName(method.ReturnType);
+            string parameters = string.Join(", ",
+                method.GetParameters().Select(p =>
+                    $"{(p.ParameterType.IsByRef ? "ref " : "")}{GetFriendlyName(p.ParameterType)} {p.Name}"));
+
+            if (returnType.StartsWith("Task")) // Handle Task-based methods
+            {
+                returnType = "void";
+                parameters = string.IsNullOrWhiteSpace(parameters)
+                    ? "AsyncCallback<" + GetGenericType(method.ReturnType) + "> callback"
+                    : parameters + ", AsyncCallback<" + GetGenericType(method.ReturnType) + "> callback";
+            }
+
+            return $"{returnType} {method.Name}({parameters});";
+        }
+
+        public static string GenerateWrapperMethodImplementation(MethodInfo method, string instanceName)
+        {
+            string returnType = GetFriendlyName(method.ReturnType);
+            string parameters = string.Join(", ",
+                method.GetParameters().Select(p =>
+                    $"{(p.ParameterType.IsByRef ? "ref " : "")}{GetFriendlyName(p.ParameterType)} {p.Name}"));
+
+            if (returnType.StartsWith("Task")) // Handle Task-based methods
+            {
+                returnType = "void";
+                string callbackType = GetGenericType(method.ReturnType);
+                string methodCall = string.IsNullOrWhiteSpace(parameters) ? "callback" : parameters + ", callback";
+                return $@"
+        public {returnType} {method.Name}({parameters}, AsyncCallback<{callbackType}> callback)
+        {{
+            Task.Run(() =>
+            {{
+                try
+                {{
+                    var result = {instanceName}.{method.Name}().Result;
+                    callback(result, null);
+                }}
+                catch (Exception ex)
+                {{
+                    callback(default, ex);
+                }}
+            }});
+        }}";
+            }
+            else
+            {
+                return $@"
+        public {returnType} {method.Name}({parameters})
+        {{
+            return {instanceName}.{method.Name}({string.Join(", ", method.GetParameters().Select(p => p.Name))});
+        }}";
+            }
+        }
+    }
+}
--- WrapperGenerator/WrapperGenerator.cs
+++ WrapperGenerator/WrapperGenerator.cs
@@ -14,12 +14,12 @@ namespace WrapperGenerator
 
             foreach (Type type in sdkAssembly.GetTypes().Where(t => t.IsPublic))
             {
-                string className = GetBaseTypeName(type) + "Wrapper";
-                string interfaceName = "I" + GetBaseTypeName(type);
+                string className = CodeGenerationUtils.GetBaseTypeName(type) + "Wrapper";
+                string interfaceName = "I" + CodeGenerationUtils.GetBaseTypeName(type);
                 string namespaceName = type.Namespace;
 
                 var namespaces = new HashSet<string> { "System", "System.Threading.Tasks" };
-                namespaces.UnionWith(GetMethodNamespaces(type));
+                namespaces.UnionWith(CodeGenerationUtils.GetMethodNamespaces(type));
 
                 string directoryPath =
                     Path.Combine(outputPath, namespaceName.Replace('.', Path.DirectorySeparatorChar));
@@ -37,112 +37,21 @@ namespace WrapperGenerator
                     writer.WriteLine("{");
                     writer.WriteLine($"    public class {className} : {interfaceName}");
                     writer.WriteLine("    {");
-                    writer.WriteLine($"        private readonly {GetFriendlyName(type)} _instance;");
+                    writer.WriteLine(
+                        $"        private readonly {CodeGenerationUtils.GetFriendlyName(type)} _instance;");
                     writer.WriteLine($"        public {className}()");
                     writer.WriteLine("        {");
-                    writer.WriteLine($"            _instance = new {GetFriendlyName(type)}();");
+                    writer.WriteLine($"            _instance = new {CodeGenerationUtils.GetFriendlyName(type)}();");
                     writer.WriteLine("        }");
 
                     foreach (MethodInfo method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance |
                                                                   BindingFlags.DeclaredOnly))
                     {
-                        string returnType = GetFriendlyName(method.ReturnType);
-                        string parameters = string.Join(", ",
-                            method.GetParameters().Select(p =>
-                                $"{(p.ParameterType.IsByRef ? "ref " : "")}{GetFriendlyName(p.ParameterType)} {p.Name}"));
-
-                        if (returnType.StartsWith("Task")) // Handle Task-based methods
-                        {
-                            returnType = "void";
-                            string callbackType = GetGenericType(method.ReturnType);
-                            string methodCall = string.IsNullOrWhiteSpace(parameters)
-                                ? "callback"
-                                : parameters + ", callback";
-                            writer.WriteLine(
-                                $"        public {returnType} {method.Name}({parameters}, AsyncCallback<{callbackType}> callback)");
-                            writer.WriteLine("        {");
-                            writer.WriteLine($"            Task.Run(() =>");
-                            writer.WriteLine("            {");
-                            writer.WriteLine("                try");
-                            writer.WriteLine("                {");
-                            writer.WriteLine($"                    var result = _instance.{method.Name}().Result;");
-                            writer.WriteLine("                    callback(result, null);");
-                            writer.WriteLine("                }");
-                            writer.WriteLine("                catch (Exception ex)");
-                            writer.WriteLine("                {");
-                            writer.WriteLine("                    callback(default, ex);");
-                            writer.WriteLine("                }");
-                            writer.WriteLine("            });");
-                            writer.WriteLine("        }");
-                        }
-                        else
-                        {
-                            writer.WriteLine($"        public {returnType} {method.Name}({parameters})");
-                            writer.WriteLine("        {");
-                            writer.WriteLine($"            return _instance.{method.Name}({parameters});");
-                            writer.WriteLine("        }");
-                        }
+                        writer.WriteLine(CodeGenerationUtils.GenerateWrapperMethodImplementation(method, "_instance"));
                     }
 
                     writer.WriteLine("    }");
                     writer.WriteLine("}");
-                }
-            }
-        }
-
-        private static string GetBaseTypeName(Type type)
-        {
-            string baseName = type.Name;
-            if (type.IsGenericType)
-            {
-                baseName = type.Name.Substring(0, type.Name.IndexOf('`'));
-            }
-
-            return baseName;
-        }
-
-        private static string GetFriendlyName(Type type)
-        {
-            if (type.IsGenericType)
-            {
-                string genericType = type.GetGenericTypeDefinition().Name;
-                genericType = genericType.Substring(0, genericType.IndexOf('`'));
-                string genericArgs = string.Join(", ", type.GetGenericArguments().Select(GetFriendlyName));
-                return $"{genericType}<{genericArgs}>";
-            }
-
-            if (type.IsByRef)
-            {
-                return GetFriendlyName(type.GetElementType());
-            }
-
-            if (type.IsInterface && type.Namespace != null && type.Namespace.StartsWith("Steamworks"))
-            {
-                return "I" + type.Name;
-            }
-
-            return type.Name;
-        }
-
-        private static string GetGenericType(Type type)
-        {
-            if (type.IsGenericType)
-            {
-                return GetFriendlyName(type.GetGenericArguments()[0]);
-            }
-
-            return "object";
-        }
-
-        private static IEnumerable<string> GetMethodNamespaces(Type type)
-        {
-            foreach (MethodInfo method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance |
-                                                          BindingFlags.DeclaredOnly))
-            {
-                yield return method.ReturnType.Namespace;
-                foreach (ParameterInfo parameter in method.GetParameters())
-                {
-                    yield return parameter.ParameterType.Namespace;
                 }
             }
         }
